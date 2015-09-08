0b5a0cce7f062f8783e32c7ddb25235c0860a7df
Khalid Aldossary <khalid@itdar.com>
2015-09-08 13:05:40 +0300

    ...

--- client/articles.js
+++ client/articles.js
@@ -1,7 +1,5 @@
 Meteor.subscribe('Articles');
 
-//Articles.insert({ title: "Hello world!", body: "This is a text body..", user: "khalid", createdAt: new Date() });
-
 Template.articles.helpers({
 	articles: function(){
 		return Articles.find({},{sort: {dateAdded: -1}});
