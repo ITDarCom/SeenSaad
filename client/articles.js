437b80d0fe74b0694bb4535b2e68580220fd5fdf
Khalid Aldossary <khalid@itdar.com>
2015-09-14 10:34:09 +0300

    ...

--- client/articles.js
+++ client/articles.js
@@ -3,3 +3,17 @@ Template.articles.helpers({
 		return Articles.find({},{sort: {createdAt: -1}});
 	}
 });
+
+Template.addPlayerForm.events({
+	'click .remove': function(){
+		Meteor.call('removeArticle', this._id);
+	}
+});
+
+
+Meteor.methods({
+	'removeArticle': function(ArticleId){
+		Articles.remove(ArticleId);
+	}
+});
+
