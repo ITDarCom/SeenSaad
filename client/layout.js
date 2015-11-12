5fcda107128b717e38b454a95c68d8d6060c0e5f
omar <omar.ite@gmail.com>
2015-11-12 12:20:15 +0200

    fix width

--- client/layout.js
+++ client/layout.js
@@ -1,20 +1,13 @@
-//accountsUIBootstrap3.setLanguage('ar'); // for Arabic
-//Accounts.ui.config({
-//	passwordSignupFields: "USERNAME_ONLY"
-//});
-
 Template.layout.events({
-
     'click .logOut': function () {
         AccountsTemplates.logout();
     },
     'click .signIn': function () {
         event.preventDefault();
-        Router.go('signIn')
+        Router.go('signIn');
     }
-})
+});
 Template.headerText.helpers({
-
     headerText: function () {
         var headers = {
             'add': 'إضافة موضوع',
