5e09572daf3a3cba65371611f0acd76f3fe3ca30
Kasem <budeir.kasem@gmail.com>
2016-03-24 18:06:05 +0200

    befor git branch omer

--- client/lib/flowRouter.js
+++ client/lib/flowRouter.js
@@ -1,7 +1,25 @@
-FlowRouter.route( '/about', {
-  action: function() {
-    
-    alert( "Okay, we're on the Terms of Service page!" );
-  },
-  
+FlowRouter.route('/about' , {
+    name:"about" ,
+    action: function(){
+
+        BlazeLayout.render('layout', {  main: "about" });
+    }
+});
+
+FlowRouter.route('/add' , {
+    name:"add" ,
+    action: function(){
+
+        BlazeLayout.render('layout', {  main: "add" });
+    }
+});
+
+
+
+FlowRouter.route('/messages' , {
+    name:"messages" ,
+    action: function(){
+
+        BlazeLayout.render('layout', {  main: "messages" });
+    }
 });
