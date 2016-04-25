668d85854cf67e2862386e2879f07bdf76a7c07b
Omar <omar.ite@gmail.com>
2016-04-25 18:22:09 +0300

    move removeArticle method to Server side

--- server/Methods.js
+++ server/Methods.js
@@ -1,3 +1,19 @@
 /**
  * Created by omar on 1/24/16.
  */
+
+Meteor.methods({
+    removeArticle: function (docId) {
+        check(docId, String);
+        var article = Articles.findOne(docId);
+        if (article && article.user == this.userId)
+            Articles.update({_id:docId},{$set: {deleted:true}},{filter:false}, function (err, result) {
+                if (!err) {
+                    if (Meteor.isClient) {
+                        Router.go('mine');
+                    }
+                }
+            });
+    }
+
+})
