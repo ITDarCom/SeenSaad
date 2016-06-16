c0556ea43d65422918bdfd4437fa128e6dc84ff2
Omar <omar.ite@gmail.com>
2016-06-16 18:34:39 +0300

    enable migration for publish

--- server/migrate.js
+++ server/migrate.js
@@ -1,22 +1,28 @@
 ///**
 // * Created by omar on 6/9/16.
 // */
+
+
 ////this migration for adding username name to old article & comments and messages documents
 //
-//Meteor.startup(function () {
-//    Articles.find({}).forEach(function (elem, val, test) {
-//        elem.username = Meteor.users.findOne(elem.user).username;
-//        Articles.update(elem._id, {$set: elem}, {validate: false});
-//    });
-//    Messages.find({}).forEach(function (elem, val, test) {
-//        elem.fromUsername = Meteor.users.findOne(elem.from).username;
-//        elem.toUsername = Meteor.users.findOne(elem.to).username;
-//        Messages.update(elem._id, {$set: elem}, {validate: false});
-//    });
-//    Comments.find({}).forEach(function (elem, val, test) {
-//
-//        elem.commenterUsername = Meteor.users.findOne(elem.commenter).username;
-//        Comments.update({_id: elem._id}, {$set: elem}, {validate: false});
-//    })
-//
-//})
+
+Meteor.startup(function () {
+    Articles.find({}).forEach(function (elem ) {
+        elem.username = Meteor.users.findOne(elem.user).username;
+        Articles.update(elem._id, {$set: elem}, {validate: false});
+    });
+    Messages.find({}).forEach(function (elem) {
+        elem.fromUsername = Meteor.users.findOne(elem.from).username;
+        elem.toUsername = Meteor.users.findOne(elem.to).username;
+        Messages.update(elem._id, {$set: elem}, {validate: false});
+    });
+    Comments.find({}).forEach(function (elem) {
+
+        elem.commenterUsername = Meteor.users.findOne(elem.commenter).username;
+        Comments.update({_id: elem._id}, {$set: elem}, {validate: false});
+    })
+
+});
+
+
+//==================================
